cd68adcf48a52360ff2cecb80c7d8376dc8575e2
Umairhhanif <159680651+Umairhhanif@users.noreply.github.com>
2024-03-30 14:58:44 +0500

    all files add

---
+++ Index.ts
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+let myBalance = 10000;  // in doller
+let myPin = 9996;
+
+console.log( chalk.blue("\n \tWelcome to your Account\t"));
+
+let pinAnswer = await inquirer.prompt([
+
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pin number :",
+    }
+])
+if (pinAnswer.pin === myPin) {
+    console.log("CORRECT PIN CODE ---- login successfully");
+
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Enter your operation :",
+            choices: [, "Withdraw", "check Balance", ]
+        }
+    ])
+}
+
+    
